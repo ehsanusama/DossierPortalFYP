4b786d3b81abe22e50e0a39ec7ff4e9ba785cfe6
ehsanusama <ehsan.usama@outlook.com>
2023-06-14 15:13:27 +0500

    Documents Viewer
    documents viewers

--- pages/generate_pdf.php
+++ pages/generate_pdf.php
@@ -1,3 +1,32 @@
+<style>
+    body {
+        padding: 80px;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+
+    .row {
+        justify-content: center;
+    }
+
+    .ctn2 p,
+    .rp p,
+    .from p,
+    .to p {
+        font-size: 19px;
+        font-weight: 700;
+        font-style: italic;
+    }
+
+    table tr td {
+        padding: 10px;
+        font-weight: 500;
+    }
+
+    tbody {
+        border: 1px solid black;
+    }
+</style>
+
 <div class="portlet light">
     <div class="portlet-body">
         <div class="row">
@@ -12,16 +41,41 @@
             <button style="font-size: 12px" class="btn btn-danger pdf_portrait_btn hidden-print">Export PDF</button>
         </div>
     </div>
+
+</div>
+
+<div class="portlet light">
     <?php if (!empty($_REQUEST['generate_pdf'])) : {
             $summary = fetchRecord($dbc, "executive_summary", 'user_id', $fetchUser['user_id']);
             $research_interests = fetchRecord($dbc, "research_interests", 'user_id', $fetchUser['user_id']);
             $personal_mission = fetchRecord($dbc, "personal_mission", 'user_id', $fetchUser['user_id']);
+            $statement_teaching = fetchRecord($dbc, "statement_teaching", 'user_id', $fetchUser['user_id']);
         } ?>
         <div class="portlet-body" id="pdfBodyPortrait">
             <div class="container">
-                <h3>Instructions:</h3>
-                <ol>
-                    <li>Submit the dossier in hard and soft copy to the HR section of Registrar office (hrm@ntu.edu.pk)</li>
+                <div class="row">
+                    <div class="col-4" style="text-align: center; width: 20%;"><img src="img/nut-img.jpg" alt="" width="150px" height="150px">
+                        <!-- logo -->
+                    </div>
+                    <div class="col-7" style=" text-align: center;  margin-top: -25px;">
+                        <h1 class="mt-5" style="font-size: 32px;">
+                            NATIONAL TEXTILE UNIVERSITY
+                        </h1>
+                        <center>
+                            <h4>Dossier Form</h4>
+                        </center>
+                        <p style="font-size: 18px;
+                    color: blue; 
+                    font-weight: 500;
+                    ">(Evaluation of Mid / End Term / Appointment of Assistant/Associate/ Full
+                            Professor)</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container" style="padding: 0 140px; margin-top: 40px;">
+                <h3 style="font-size: 25px;">Instructions:</h3>
+                <ol type="i" style="padding: 0 71px;">
+                    <li>Submit the dossier in hard and soft copy to the HR section of Registrar office <a href="#" style="font-weight: 500;">(hrm@ntu.edu.pk)</a></li>
                     <li>Attach all necessary relevant documents.</li>
                     <li>Attach first page of published papers</li>
                     <li>Extra sheets/columns may be added of the same formats if required. </li>
@@ -30,76 +84,97 @@
                         promotion/appointment as per HEC criteria need to submit an application along
                         with dossier. </li>
                 </ol>
-                <h3>Reporting period: From: 1 January 202217 to 31 December 2022</h3>
-                <div class="row">
-                    <div class="col-4"></div>
-                    <div class="col-2">(dd-mm-yyyy) </div>
-                    <div class="col-2"> (dd-mm-yyyy)
+
+                <div class="container ctn2" style="display: flex; padding: 0;">
+                    <div class="rp">
+                        <p>Reporting period:</p>
                     </div>
-                    <div class="col-4"></div>
+                    <div class="" style="text-align: end; padding: 0 15px;">
+                        <p>From: 1 January 2022</p>
+                        <p style="margin-top: -13px;">(dd-mm-yyyy)</p>
+                    </div>
+                    <p>to</p>
+                    <div class="" style="text-align: end; padding: 0 15px;">
+                        <p>31 December 2022</p>
+                        <p style="margin-top: -13px;">(dd-mm-yyyy)</p>
+                    </div>
+
                 </div>
-                <h3 class="mt-5">TO BE FILLED BY THE APPLICANT IN BLOCK LETTERS</h3>
-                <hr>
-                <table class="border" width="100%" style="border-color: black;">
+
+
+
+
+
+                <h3 class="mt-2" style="font-size: 21px; text-decoration: underline;">TO BE FILLED BY THE APPLICANT IN BLOCK LETTERS</h3>
+                <hr style="    opacity: 1;margin: 9px 0;">
+                <table class="border" width="100%">
                     <tr>
-                        <th style="font-size: 25px; background-color: gray;" colspan="3"><strong>A:PERSONAL DETAILS</strong></th>
+                        <th style="font-size: 18px;
+                    background-color: #ddd;
+                    padding: 15px 4px;
+                    font-weight: 500;
+                    " colspan="3">A: PERSONAL DETAILS
+                        </th>
 
                     </tr>
                     <tr>
-                        <td>Name:ABC_</td>
-                        <td> Father name:ABC_</td>
+                        <td style="width: 39%;">Name:_________________</td>
+                        <td> Father name: ______________</td>
                     </tr>
 
 
                     <tr>
-                        <td>DOB: 00 MAY 0000</td>
-                        <td> Age: 43 YEARS </td>
-                        <td>CNIC #</td>
+                        <td>DOB: ________________</td>
+                        <td style="width: 0;"> Age: ____________ </td>
+                        <td style="position: absolute;
+                    left: 53%;">CNIC #</td>
                     </tr>
                     <tr>
-                        <td>Domicile: FAISALABAD</td>
-                        <td> Nationality: __________
+                        <td>Domicile: ______________</td>
+                        <td> Nationality: _______________
                         </td>
 
                     </tr>
 
                     <tr>
-                        <td>Designation: Pay Scale: 19-TTS</td>
+                        <td>Designation: _______________</td>
                     </tr>
 
 
                     <tr>
-                        <td>Department: COMPUTER SCIENCE </td>
-                        <td> Date of Appointment at NTU: 16 JANUARY 0000
+                        <td>Department: _________________ </td>
+                        <td style="width: 50%;"> Date of Appointment at NTU: _______________
                         </td>
 
                     </tr>
 
                     <tr>
-                        <td>Total Post PhD Experience: 0 YEARS & 0 MONTH </td>
+                        <td>Total Post PhD Experience: __________ </td>
                     </tr>
 
                     <tr>
-                        <td>Total service on TTS: 6 YEAR </td>
+                        <td>Total service on TTS: ___________ </td>
                     </tr>
 
                     <tr>
-                        <td>Total service as Assistant Professor: 6 YEAR</td>
+                        <td>Total service as Assistant Professor: __________</td>
                     </tr>
 
                     <tr>
-                        <td>Mid Term review (if applicable): YES COMPLETE </td>
+                        <td style="width: 31%;">Mid Term review (if applicable): _____________ </td>
                     </tr>
 
                     <tr>
                         <td>Postal Address:_________________ </td>
                     </tr>
                     <tr>
-                        <td>Permanent Address:______________________________ </td>
+                        <td>Permanent Address: <br>
+                            <p style="position: absolute;top: 146.5%;left: 28%;">__________________________________________________</p>
+                        </td>
                     </tr>
 
                     <tr>
-                        <td>Email: ____________________ </td>
+                        <td>Email: ______________________ </td>
                         <td>Telephone (Res/office). Cell no. _____________________________
                         </td>
                     </tr>
@@ -109,13 +184,10 @@
 
                 <!-- container -->
             </div>
-            <div class="container">
+            <div class="container" style="padding: 0 140px; margin-top: 40px;">
                 <h3>Table of Contents</h3>
-
                 <ol>
                     <li> Highlights of the profile......................................................................................................................................................4 </li>
-
-
                     <ol>
                         <li>Executive Summary..................................................................................................................................................4</li>
                         <li>Research....................................................................................................................................................................6</li>
@@ -186,6 +258,8 @@
                 <h4>(1.1) Executive Summary</h4>
                 <?= @$summary['summary'] ?>
             </div>
+            <!-------------------------------------->
+
             <div class="container">
                 <h3>(1.2) Research</h3>
                 <h4>(1.2)1. Research interests</h4>
@@ -230,6 +304,7 @@
                     </table>
                 </div>
             </div>
+            <!-------------------------------------->
             <div class="container">
                 <h3>(1.3) Academic</h3>
                 <div class="table-responsive">
@@ -271,6 +346,7 @@
                 </div>
 
             </div>
+            <!-------------------------------------->
             <div class="container">
                 <h3>(1.4) Other contributions</h3>
                 <div class="table-responsive">
@@ -312,10 +388,14 @@
                     </table>
                 </div>
             </div>
+            <!-------------------------------------->
+
             <div class="container">
                 <h3>(2) Personal Mission</h3>
                 <?= $personal_mission['summary'] ?>
             </div>
+            <!-------------------------------------->
+
             <div class="container">
                 <h3>(3) Academic Qualification</h3>
                 <div class="table-responsive">
@@ -366,6 +446,8 @@
                     </table>
                 </div>
             </div>
+            <!-------------------------------------->
+
             <div class="container">
                 <h3>(4) Professional Experience</h3>
                 <div class="table-responsive">
@@ -399,6 +481,177 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
+            <!-------------------------------------->
+
+            <div class="container">
+                <h2>(5) Teaching Profile</h2>
+                <h3>(5.1) Statement of Teaching:</h3>
+                <?= $statement_teaching['statement_teaching_text'] ?>
+            </div>
+            <!-------------------------------------->
+
+            <div class="container">
+                <h3>(5.2) Detail of Courses Taught</h3>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Course Title </th>
+                                <th class="text-center">Credit Hours </th>
+                                <th class="text-center">Teaching Hours (since 20xx) </th>
+                                <th class="text-center">PhD/MS/BS </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM taught_course_details WHERE user_id = $fetchUser[user_id]";
+                            $q = mysqli_query($dbc, $sql);
+                            while ($row = mysqli_fetch_assoc($q)) :
+                            ?>
+                                <tr>
+                                    <td><?= $row['title'] ?></td>
+                                    <td><?= $row['credit_hour'] ?></td>
+                                    <td><?= $row['teaching_hour'] ?></td>
+                                    <td><?= $row['phd_ms_bs'] ?></td>
+                                </tr>
+
+                            <?php
+                            endwhile;
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-------------------------------------->
+
+            <div class="container">
+                <h3>(5.3) Detail of New Courses Developed</h3>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Course Title </th>
+                                <th class="text-center">Credit Hours </th>
+                                <th class="text-center">PhD/MS/BS </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM develop_course_details WHERE user_id = $fetchUser[user_id]";
+                            $q = mysqli_query($dbc, $sql);
+                            while ($row = mysqli_fetch_assoc($q)) :
+                            ?>
+                                <tr>
+                                    <td><?= $row['title'] ?></td>
+                                    <td><?= $row['credit_hour'] ?></td>
+                                    <td><?= $row['phd_ms_bs'] ?></td>
+                                </tr>
+
+                            <?php
+                            endwhile;
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-------------------------------------->
+
+            <div class="container">
+                <h3>(5.4) Curriculum Development & Review</h3>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Role </th>
+                                <th class="text-center">Organization </th>
+                                <th class="text-center">Duties </th>
+                                <th class="text-center">From </th>
+                                <th class="text-center">To </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM curriculum_develop WHERE user_id = $fetchUser[user_id]";
+                            $q = mysqli_query($dbc, $sql);
+                            while ($row = mysqli_fetch_assoc($q)) :
+                            ?>
+                                <tr>
+                                    <td><?= $row['institute'] ?></td>
+                                    <td><?= $row['position'] ?></td>
+                                    <td><?= $row['duties'] ?></td>
+                                    <td><?= $row['year_from'] ?></td>
+                                    <td><?= $row['year_to'] ?></td>
+                                </tr>
+
+                            <?php
+                            endwhile;
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-------------------------------------->
+
+            <div class="container">
+                <h3>(5.5) Trainings & Certificates (Attended) / Conducted</h3>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="col-md-4" class="text-center">Details </th>
+                                <th class="text-center">File </th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM traning_conducted WHERE user_id = $fetchUser[user_id]";
+                            $q = mysqli_query($dbc, $sql);
+                            while ($row = mysqli_fetch_assoc($q)) :
+                            ?>
+                                <tr>
+                                    <td colspan="col-md-4"><?= $row['details'] ?></td>
+                                    <td><?= $row['file'] ?></td>
+
+                                </tr>
+
+                            <?php
+                            endwhile;
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-----------------Annexure 1:--------------------->
+
+            <div class="container">
+                <h3>Annexure 1:</h4>
+                    <h4>Administrative / Non-academic departmental duties</h4>
+            </div>
+            <!-----------------Annexure 2:--------------------->
+
+            <div class="container">
+                <h3>Annexure 2:</h4>
+                    <h4>Administrative / Non-academic departmental duties</h4>
+            </div>
+            <!-----------------Annexure 3:--------------------->
+
+            <div class="container">
+                <h3>Annexure 3:</h4>
+                    <h4>Administrative / Non-academic departmental duties</h4>
+            </div>
+            <!-----------------Annexure 4:--------------------->
+
+            <div class="container">
+                <h3>Annexure 4:</h4>
+                    <h4>Administrative / Non-academic departmental duties</h4>
+            </div>
+            <!-----------------Annexure 5:--------------------->
+
+            <div class="container">
+                <h3>Annexure 5:</h4>
+                    <h4>Administrative / Non-academic departmental duties</h4>
             </div>
         </div>
     <?php endif; ?>
