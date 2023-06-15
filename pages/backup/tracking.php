29cb4c412719a34cc99441c6d18e3442d0a69d05
ehsanusama <ehsan.usama@outlook.com>
2023-06-15 14:22:37 +0500

    Remove Extra Files

--- pages/backup/tracking.php
+++
@@ -1,5 +0,0 @@
-<div class="portlet light" style="height:100vh">
-    <div class="portlet-body " style="height:90vh">
-        <iframe src="https://www.attendezz.com/dashboard/map_module/overall.php?business_id=7" style="width:100%;height:80vh" frameborder="0"></iframe>
-    </div>
-</div>
