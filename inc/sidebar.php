2b3cf22c061a865a69c3d91515d560c0af8184e7
ehsanusama <ehsan.usama@outlook.com>
2023-06-15 13:30:13 +0500

    sidebar-updated

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -7,7 +7,7 @@
                     <span class="title">Dashboard</span>
                 </a>
             </li>
-            <?php if (!empty($_SESSION['business'])) : ?>
+            <?php if (empty($_SESSION['business'])) : ?>
                 <?php foreach (array_unique($parents) as  $p) :
                     $unique_parent = fetchRecord($dbc, "menus", "id", $p);
                 ?>
@@ -73,8 +73,38 @@
                     <?php endforeach; ?>
                 <?php endif; ?>
             <?php endif; ?>
-            <?php if ($getRoleEmployee >= 1) : ?>
-            <?php endif; ?>
+            <?php if ($getRoleEmployee >= 1) :
+                foreach (array_unique($parents) as  $p) :
+                    $unique_parent = fetchRecord($dbc, "menus", "id", $p);
+            ?>
+                    <li class="nav-item">
+                        <a href="javascript:;" class="nav-link nav-toggle">
+                            <i class="<?= $unique_parent['icon'] ?>"></i>
+                            <span class="title"><?= ucwords($unique_parent['title']) ?></span>
+                            <span class="arrow open"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <?php foreach ($files as  $value) :
+                                $filename = $value . ".php";
+                                $q = mysqli_query($dbc, "SELECT * FROM menus WHERE parent_id='$p' AND page='$filename'");
+                                if (mysqli_num_rows($q) == 1) :
+                                    $navigation = fetchRecord($dbc, "menus", "page", $filename);
+                                    if (empty($navigation['parent_id']) and $navigation['page'] == "#") {
+                                        continue;
+                                    }
+                            ?>
+                                    <li class="nav-item">
+                                        <a href="index.php?nav=<?= base64_encode($value) ?>" class="nav-link ">
+                                            <i class="<?= $navigation['icon'] ?>"></i>
+                                            <span class="title"><?= ucwords($navigation['title']) ?></span>
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+            <?php endforeach;
+            endif; ?>
             <li class="nav-item">
                 <a href="index.php?nav=<?= base64_encode('profile') ?>&business=<?= @$_SESSION['business'] ?>" class="nav-link">
                     <i class="fa fa-cog"></i>
